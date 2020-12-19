89f77eb565c4f1a7c275b3a8031b1862c9e82588
shu <shuguoli68@163.com>
2020-12-19 23:31:18 +0800

    新建项目

---
+++ src/main/kotlin/com/example/liblogserver/LiblogserverApplication.kt
@@ -0,0 +1,11 @@
+package com.example.liblogserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LiblogserverApplication
+
+fun main(args: Array<String>) {
+    runApplication<LiblogserverApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/liblogserver/LiblogserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.liblogserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LiblogserverApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
