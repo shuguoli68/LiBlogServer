f048cf2091561a9a6420d874b18be7ee3c38b435
shu <shuguoli68@163.com>
2021-01-04 23:12:32 +0800

    user接口

---
+++ src/main/kotlin/com/example/liblogserver/service/UserService.kt
@@ -0,0 +1,9 @@
+package com.example.liblogserver.service
+
+/**
+ * Created on 2021/1/4 22:12
+ * @author shuguo
+ */
+interface UserService {
+    fun checkUser(name:String, password:String)
+}
---
+++ src/main/kotlin/com/example/liblogserver/service/UserServiceImpl.kt
@@ -0,0 +1,9 @@
+package com.example.liblogserver.service
+
+/**
+ * Created on 2021/1/4 22:14
+ * @author shuguo
+ */
+class UserServiceImpl {
+
+}
